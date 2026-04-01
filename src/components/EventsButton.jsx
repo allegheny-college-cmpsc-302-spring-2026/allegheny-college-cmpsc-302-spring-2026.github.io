6a646e58d41d0904b41c892a3e31947b73e86563
dluman <dluman@allegheny.edu>
2026-04-01 14:43:47 -0400

    Updating the short bit about `evt`

--- src/components/EventsButton.jsx
+++ src/components/EventsButton.jsx
@@ -3,12 +3,12 @@ import React, {useState} from 'react';
 const EventsButton = () => {
   const handleClick = (evt) => {
     alert("Button was clicked!");
-    console.log("Button was clicked!", evt);
+    console.log(evt.target.className);
   };
 
   return (
     // 2. Pass the function reference to onClick
-    <button onClick={handleClick}>
+    <button class = "submitButton" onClick={handleClick}>
       Click Me
     </button>
   );
